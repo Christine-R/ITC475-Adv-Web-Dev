7ea936cdcbe8439d6f3f181a411d4a11b5f5fdc5
Christine R <26476374+Christine-R@users.noreply.github.com>
2022-08-31 12:24:11 -0400

    Basic setup
    Set up basic HTML and CSS

---
+++ MegaTravel/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8" />
+        <title>Mega Travel</title>
+        <meta name="description" content="ITC 475 assignment: Mega Travel website" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Rajdhani&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+    
+        <div id="wrapper">
+
+            <!-- Menu -->
+            <div id="headerbox">
+                <p class="welcome">Mega Travel<br>
+                Coming soon</p>
+            </div>
+ 
+        </div>
+       
+
+    </body>
+</html>
